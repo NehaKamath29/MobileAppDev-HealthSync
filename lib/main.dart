10e6987c0561c2e2488378af3f47fec79e8f9364
NehaKamath29 <nehabhaskark.cs21@bmsce.ac.in>
2024-01-12 20:08:22 +0530

    PersonalProfile-Done

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:healthsync_app/utils/utils.dart';
 //import 'package:healthsync_app/pages/login.dart';
 //import 'package:healthsync_app/pages/signup.dart';
-import 'package:healthsync_app/pages/profile_landing.dart';
+//import 'package:healthsync_app/pages/profile_landing.dart';
+import 'package:healthsync_app/pages/personal_profile.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget 
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget
     },*/
     home: const Scaffold(
 		body: SingleChildScrollView(
-		child: ProfilePage(),  //Give the class name of the page you want to be displayed and import the respective file. After navigations,
+		child: PersonalProfile(),  //Give the class name of the page you want to be displayed and import the respective file. After navigations,
                            //only the landing page class name will be given ig.
 		),
 		),
