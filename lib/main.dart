f76f5e9ec117d53a3af2fb6ca6f2b67939d2931c
Harshitha <harshitha.cs21@bmsce.ac.in>
2024-01-30 15:33:03 +0530

    Bottom Nav Bar Added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:healthsync_app/pages/ba_doc_info.dart';
 import 'package:healthsync_app/pages/forgetpass_landing.dart';
 import 'package:healthsync_app/pages/health_profile.dart';
+import 'package:healthsync_app/pages/notifications.dart';
 import 'package:healthsync_app/pages/profile_landing.dart';
 import 'package:healthsync_app/utils/utils.dart';
 import 'package:healthsync_app/pages/login.dart';
@@ -53,22 +54,66 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter',
       debugShowCheckedModeBanner: false,
-      scrollBehavior: MyCustomScrollBehavior(),
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(),
+    );
+  }
+}
 
-      /*routes:{
-      '/signup':(context)=>const SignupClass(),
-      //'/login':(context)=>const LoginClass(),
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-    },*/
-      home: const Scaffold(
-        body: SingleChildScrollView(
-          child:
-              BaSlotClass(), //Give the class name of the page you want to be displayed and import the respective file. After navigations,
-          //only the landing page class name will be given ig.
-        ),
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    HomeClass(),
+    MedBillApp(),
+    MedHistoryNR(),
+    NotifClass(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        // backgroundColor: Colors.blue, // Set the background color
+        selectedItemColor: Colors.blue, // Set the color of the selected item
+        unselectedItemColor:
+            Colors.black, // Set the color of the unselected items
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/navhome.png')),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/navbill.png')),
+            label: 'Medical Bill',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/navhistory.png')),
+            label: 'MedicalHistory',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/navothers.png')),
+            label: 'Notifications',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
